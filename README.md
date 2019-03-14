c5f2cda0e67fbfde5301b75e03a44b6b51654bc4
Christian Sakai <christianmsakai@gmail.com>
2019-03-14 11:44:59 -0400

    Add env and database

---
+++ README.md
@@ -0,0 +1,3 @@
+Dependencies: 
+- `cargo install diesel_cli --no-default-features --features postgres`
+- `brew install postgres`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#[macro_use] extern crate diesel;
+
 use std::env;
 use std::collections::HashMap;
 
@@ -14,6 +16,12 @@ use actix_web::{
     Result,
 };
 use askama::Template;
+use dotenv::dotenv;
+use diesel::Connection;
+use diesel::pg::PgConnection;
+
+mod schema;
+mod models;
 
 #[derive(Template)]
 #[template(path = "base.html")]
@@ -44,10 +52,33 @@ fn index(query: Query<HashMap<String, String>>) -> Result<HttpResponse> {
     Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
 
+fn establish_connection() -> PgConnection {
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+
+    PgConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url))
+}
+
 fn main() {
-    env::set_var("RUST_LOG",  "actix_web=info");
+    dotenv().ok();
     env_logger::init();
 
+    // DB
+    let connection = establish_connection();
+    // let results = posts.filter(published.eq(true))
+    //     .limit(5)
+    //     .load::<Post>(&connection)
+    //     .expect("Error loading posts");
+
+    // println!("Displaying {} posts", results.len());
+    // for post in results {
+    //     println!("{}", post.title);
+    //     println!("----------\n");
+    //     println!("{}", post.body);
+    // }
+
+    // Web
     let sys = System::new("todo");
 
     server::new(|| {
