1b6011aa0ac9410ba1675eadbce310723830c46f
Christian Sakai <christianmsakai@gmail.com>
2019-03-15 16:36:33 -0400

    Basic CRUD pattern

--- src/main.rs
+++ src/main.rs
@@ -36,23 +36,87 @@ struct Base;
 struct Index;
 
 #[derive(Template)]
-#[template(path = "user.html")]
-struct UserTemplate<'a> {
-    name: &'a str,
-    text: &'a str,
+#[template(path = "todo/list.html")]
+struct TodoList {
+    todos: Vec<TodoShow>,
+} 
+
+#[derive(Template)]
+#[template(path = "todo/create.html")]
+struct TodoCreate;
+
+#[derive(Template)]
+#[template(path = "todo/edit.html")]
+struct TodoEdit;
+
+#[derive(Template)]
+#[template(path = "todo/show.html")]
+struct TodoShow {
+    id: i32,
+    title: String,
+    description: String,
 }
 
-fn index(query: Query<HashMap<String, String>>) -> Result<HttpResponse> {
-    let s = if let Some(name) = query.get("name") {
-        UserTemplate {
-            name: name,
-            text: "Welcome!",
-        }.render()
-            .unwrap()
-    } else {
-        Index.render().unwrap()
-    };
+// #[derive(Template)]
+// #[template(path = "user.html")]
+// struct UserTemplate<'a> {
+//     name: &'a str,
+//     text: &'a str,
+// }
 
+// fn index(query: Query<HashMap<String, String>>) -> Result<HttpResponse> {
+//     let s = if let Some(name) = query.get("name") {
+//         UserTemplate {
+//             name: name,
+//             text: "Welcome!",
+//         }.render()
+//             .unwrap()
+//     } else{
+//         Index.render().unwrap()
+//     };
+
+//     Ok(HttpResponse::Ok().content_type("text/html").body(s))
+// }
+
+fn index(query: Query<HashMap<String, String>>) -> Result<HttpResponse> {
+    let s = Index.render().unwrap();
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
+}
+
+fn list(query: Query<HashMap<String, String>>) -> Result<HttpResponse> {
+    let s = TodoList {
+        todos: vec![
+            TodoShow {
+                id: 1,
+                title: "Hello World".to_string(),
+                description: "Hello worlddd duuuudee".to_string(),
+            },
+            TodoShow {
+                id: 2,
+                title: "Hello World 2".to_string(),
+                description: "Hello worlddd duuuudee 2".to_string(),
+            },
+        ],
+    }.render().unwrap();
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
+}
+
+fn create(query: Query<HashMap<String, String>>) -> Result<HttpResponse> {
+    let s = Index.render().unwrap();
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
+}
+
+fn edit(query: Query<HashMap<String, String>>) -> Result<HttpResponse> {
+    let s = TodoEdit.render().unwrap();
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
+}
+
+fn show(query: Query<HashMap<String, String>>) -> Result<HttpResponse> {
+    let s = TodoShow {
+        id: 1,
+        title: "Hello World".to_string(),
+        description: "Hello worlddd duuuudee".to_string(),
+    }.render().unwrap();
     Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
 
@@ -72,7 +136,11 @@ fn main() {
                 "/static",
                 fs::StaticFiles::new("./static/").unwrap(),
             )
-            .resource("/", |r| r.method(http::Method::GET).with(index)) 
+            .route("/", http::Method::GET, index) 
+            .route("/todo", http::Method::GET, list) 
+            .route("/todo/create", http::Method::GET, create) 
+            .route("/todo/{id}", http::Method::GET, show) 
+            .route("/todo/{id}/edit", http::Method::GET, edit) 
     })
     .bind("127.0.0.1:8080")
     .unwrap()
